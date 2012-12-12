699991dc61c2e42f80b12c005a01a8c69c2b33aa
Bryan Ricker <bricker88@gmail.com>
2012-12-12 01:09:41 -0800

    Move config stuff out of root file

--- lib/npr.rb
+++ lib/npr.rb
@@ -1,48 +1,13 @@
-require "activesupport"
-
 require "npr/version"
-require "npr/config"
+require "npr/configuration"
 require "npr/errors"
 
 module NPR
-  #-------------------
   
-  class << self
-    attr_reader :config
-
-    #-------------------
-    # Pass a block to configure NPR globally.
-    #
-    # Options configured globally will be
-    # passed in to every API request. They can
-    # still be overridden on a per-query basis.
-    #
-    # Example:
-    #
-    #   NPR.configure do |config|
-    #     config.apiKey         = "YOUR_API_KEY"
-    #     config.sort           = "date descending"
-    #     config.output         = "NPRML"
-    #     config.requiredAssets = "text"
-    #   end
-    #
-    def configure
-      yield config
-      config
-    end
-    
-    #-------------------
-    
-    private
-
-    #-------------------
-    
-    def config
-      @config ||= NPR::Config.new
-    end
-  end
 end # NPR
 
+require "npr/client"
+require "npr/list"
 require "npr/story"
 require "npr/image"
 require "npr/audio"
