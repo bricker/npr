0e5d9f1ed95bb376ac5ae0b30d1ae6a6fa0078b5
Bryan Ricker <bricker88@gmail.com>
2012-12-12 20:33:09 -0800

    Add Paragraph class

---
+++ lib/npr/paragraph.rb
@@ -0,0 +1,11 @@
+##
+# NPR::Paragraph
+#
+# NPR splits every paragraph into separate
+# database entries. This object will represent
+# a single paragraph for a story.
+#
+module NPR
+  class Paragraph
+  end
+end
---
+++ spec/unit/paragraph_spec.rb
@@ -0,0 +1,5 @@
+require "spec_helper"
+
+describe NPR::Paragraph do
+  pending
+end
