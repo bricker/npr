0b461fd18d9947123855700cdd0528dffbd622e2
Bryan Ricker <bricker88@gmail.com>
2012-12-14 13:07:41 -0800

    More specs

--- spec/unit/entity/link_spec.rb
+++ spec/unit/entity/link_spec.rb
@@ -1,5 +1,16 @@
 require "spec_helper"
 
 describe NPR::Entity::Link do
-  pending
+  before :each do
+    @link = NPR::Entity::Link.new({"$type" => "google.com", "type" => "html"})
+  end
+  
+  it "sets attributes" do
+    @link.type.should eq "html"
+    @link.content.should eq "google.com"
+  end
+  
+  it "uses @content for to_s" do
+    @link.to_s.should eq "google.com"
+  end
 end
--- spec/unit/entity/story_spec.rb
+++ spec/unit/entity/story_spec.rb
@@ -1,9 +1,108 @@
 require "spec_helper"
 
 describe NPR::Entity::Story do
+  config!(:apiKey => "key")
+  
+  #--------------------------
+  
+  describe "relations" do
+    before :each do
+      @story = mock_response "json/01_story_full_media.json" do
+        NPR::Story.find(999)
+      end
+    end
+    
+    it "sets up images" do
+      @story.images.should_not be_empty
+      @story.images.first.should be_a NPR::Entity::Image
+    end
+    
+    it "sets up bylines" do
+      @story.bylines.should_not be_empty
+      @story.bylines.first.should be_a NPR::Entity::Byline
+    end
+    
+    it "sets up organizations" do
+      @story.organizations.should_not be_empty
+      @story.organizations.first.should be_a NPR::Entity::Organization
+    end
+    
+    it "sets up links" do
+      @story.links.should_not be_empty
+      @story.links.first.should be_a NPR::Entity::Link
+    end
+    
+    it "sets up related_links" do
+      @story.related_links.should_not be_empty
+      @story.related_links.first.should be_a NPR::Entity::RelatedLink
+    end
+    
+    it "sets up pull_quotes" do
+      # The fixture doesn't have any pull quotes
+      # TODO Get a fixtute with pull quotes
+      @story.pull_quotes.should eq []
+    end
+  end
+  
+  #--------------------------
+  
+  describe "attributes" do
+    before :each do
+      @story = mock_response "json/01_story_full_media.json" do
+        NPR::Story.find(999)
+      end
+    end
+    
+    it "generates title" do
+      @story.title.should match /Ravi Shankar/
+    end
+    
+    it "generates partnerId" do
+      @story.partnerId.should be_a Integer
+    end
+    
+    it "generates subtitle" do
+      @story.subtitle.should be_a String
+    end
+    
+    it "generates shortTitle" do
+      @story.shortTitle.should be_a String
+    end
+    
+    it "generates miniTeaser" do
+      @story.miniTeaser.should match /sitar/
+    end
+    
+    it "generates slug" do
+      @story.slug.should match /Record/
+    end
+    
+    it "generates storyDate" do
+      @story.storyDate.should be_a Time
+    end
+    
+    it "generates pubDate" do
+      @story.pubDate.should be_a Time
+    end
+    
+    it "generates lastModifiedDate" do
+      @story.lastModifiedDate.should be_a Time
+    end
+    
+    it "generates keywords" do
+      @story.keywords.should be_a String
+    end
+    
+    it "generates priorityKeywords" do
+      @story.priorityKeywords.should be_a String
+    end
+    
+    it "generates fullText" do
+      @story.fullText.should match /Ravi Shankar/
+    end
+  end
+  
   describe "::find" do
-    config!(:apiKey => "key")
-
     it "instantiates a new story" do
       story = mock_response "json/01_story_full_media.json" do
         NPR::Entity::Story.find(167019577)
@@ -45,9 +144,7 @@ describe NPR::Entity::Story do
 
   #--------------------
 
-  describe "::find_by_id" do
-    config!(:apiKey => "key")
-    
+  describe "::find_by_id" do    
     context "with messages in the response" do
       it "returns nil" do
         response = mock_response "json/04_invalid_id.json" do
