dc65d7ad370327fea81da70133f1f571a54e5105
Bryan Ricker <bricker88@gmail.com>
2012-12-14 15:07:45 -0800

    Setup Organization model

--- lib/npr/entity/organization.rb
+++ lib/npr/entity/organization.rb
@@ -4,7 +4,15 @@
 module NPR
   module Entity
     class Organization < Base
-      attr_accessor :name, :website
+      attr_accessor :orgId, :orgAbbr
+      shallow_attribute "name", "website"
+      
+      def initialize(json)
+        extract_shallow_attributes(json)
+        
+        @orgId   = json["orgId"].to_i
+        @orgAbbr = json["orgAbbr"]
+      end
     end # Organization
   end # Entity
 end # NPR
--- spec/unit/entity/organization_spec.rb
+++ spec/unit/entity/organization_spec.rb
@@ -1,5 +1,29 @@
 require "spec_helper"
 
 describe NPR::Entity::Organization do
-  pending
+  json_fixture do
+    <<-JSON
+      {
+        "orgId": "1",
+        "orgAbbr": "NPR",
+        "name": {
+          "$text": "National Public Radio"
+        },
+        "website": {
+          "$text": "http://www.npr.org/"
+        }
+      }
+    JSON
+  end
+    
+  before :each do
+    @organization = NPR::Entity::Organization.new(@fixture)
+  end
+  
+  it "sets up attributes" do
+    @organization.name.should match /National/
+    @organization.website.should match /npr\.org/
+    @organization.orgId.should eq 1
+    @organization.orgAbbr.should eq "NPR"
+  end
 end
