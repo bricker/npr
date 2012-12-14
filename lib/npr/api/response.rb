145844a5d2a4451c74d21119696b4c1cfae332a8
Bryan Ricker <bricker88@gmail.com>
2012-12-14 01:32:58 -0800

    Add messages relations to API::Response

--- lib/npr/api/response.rb
+++ lib/npr/api/response.rb
@@ -6,19 +6,21 @@
 module NPR
   module API
     class Response
+      include NPR::Concern::Relation
+      
+      has_many "messages", :key => "message", :class_name => NPR::API::Message
       attr_reader :raw, :version, :messages, :list
       
       def initialize(response)
+        create_relations(response)
+        
         @raw      = response
         @version  = response.body["version"]
         @list     = NPR::Entity::List.new(response.body["list"])
         
-        messages = []
         Array.wrap(response.body["message"]).each do |message|
-          messages.push NPR::API::Message.new(message)
+          @messages.push NPR::API::Message.new(message)
         end
-        
-        @messages = messages
       end
     end # Response
   end # API
