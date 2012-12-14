2f9e7d84662c40d49329751dcf4b6d465b8d5302
Bryan Ricker <bricker88@gmail.com>
2012-12-14 14:50:35 -0800

    Helper for inline, partial fixtures

---
+++ spec/support/fixture_helper.rb
@@ -0,0 +1,23 @@
+##
+# FixtureHelper
+#
+# Helps with doing inline fixtures for JSON strings
+#
+module FixtureHelper
+  def self.included(base)
+    base.extend ClassMethods
+  end
+  
+  module ClassMethods
+    def json_fixture(&block)
+      before :all do
+        @json = yield
+        @fixture = JSON.parse(@json)
+      end
+      
+      after :all do
+        @json, @fixture = nil, nil
+      end
+    end
+  end
+end
