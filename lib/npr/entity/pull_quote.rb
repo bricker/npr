a3ce70dcecdf668a5600144c383e391dd509527f
Bryan Ricker <bricker88@gmail.com>
2012-12-14 15:26:57 -0800

    Setup pullQuote model + specs

--- lib/npr/entity/pull_quote.rb
+++ lib/npr/entity/pull_quote.rb
@@ -4,7 +4,13 @@
 module NPR
   module Entity
     class PullQuote < Base
-      attr_accessor :person, :date
+      attr_accessor :id
+      shallow_attribute "text", "person", "date"
+      
+      def initialize(json)
+        extract_shallow_attributes(json)
+        @id = json["id"].to_i
+      end
     end # PullQuote
   end # Entity
 end # NPR
--- spec/unit/entity/pull_quote_spec.rb
+++ spec/unit/entity/pull_quote_spec.rb
@@ -1,5 +1,29 @@
 require "spec_helper"
 
 describe NPR::Entity::PullQuote do
-  pending
+  json_fixture do
+    <<-JSON
+      {
+        "id": "162081891",
+        "text": {
+          "$text": "Unstructured time outdoors is becoming a thing of the past."
+        },
+        "person": {
+          "$text": "Chelsea Benson"
+        },
+        "date": {}
+      }
+    JSON
+  end
+  
+  before :each do
+    @pull_quote = NPR::Entity::PullQuote.new(@fixture)
+  end
+  
+  it "sets up attributes" do
+    @pull_quote.id.should be_a Integer
+    @pull_quote.text.should match /Unstructured/
+    @pull_quote.person.should match /Chelsea/
+    @pull_quote.date.should eq nil # no date information
+  end
 end
