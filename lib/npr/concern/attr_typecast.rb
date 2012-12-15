c1d88f1d3872d15ba3facf21fb87fcd1b7d574f6
Bryan Ricker <bricker88@gmail.com>
2012-12-14 22:57:02 -0800

    Refactor Typecasting to use custom methods

--- lib/npr/concern/attr_typecast.rb
+++ lib/npr/concern/attr_typecast.rb
@@ -7,22 +7,16 @@ module NPR
   #------------------
   # Attributes that are being typecast to Ruby classes
   ATTR_TYPES = {
-    "id"               => Fixnum,
-    "partnerId"        => Fixnum,
-    "storyDate"        => Time, 
-    "pubDate"          => Time,
-    "lastModifiedDate" => Time,
-    "showDate"         => Time,
-    "date"             => Time,
-    "segNum"           => Fixnum,
-    "num"              => Fixnum
-  }
-  
-  #------------------
-  # How to typecast
-  CASTERS = {
-    Fixnum => lambda { |a| a.to_i },
-    Time   => lambda { |a| !a.empty? ? Time.parse(a) : nil }
+    "id"               => :string_to_i,
+    "partnerId"        => :string_to_i,
+    "storyDate"        => :string_time_parse, 
+    "pubDate"          => :string_time_parse,
+    "lastModifiedDate" => :string_time_parse,
+    "showDate"         => :string_time_parse,
+    "date"             => :string_time_parse,
+    "segNum"           => :string_to_i,
+    "num"              => :string_to_i,
+    "timestamp"        => :string_time_at
   }
   
   module Concern
@@ -31,20 +25,34 @@ module NPR
       private
       
       #------------------
+      # Typecasting methods
+      def string_to_i(value)
+        value.to_i
+      end
+      
+      #------------------
+      
+      def string_time_parse(value)
+        !value.empty? ? Time.parse(value) : nil
+      end
+      
+      #------------------
+      
+      def string_time_at(value)
+        Time.at(value.to_f)
+      end
+      
+      
+      #------------------
+      #------------------
       
       def attr_typecast(key, value)
-        if type = NPR::ATTR_TYPES[key]
-          typecast(value, type)
+        if method = NPR::ATTR_TYPES[key]
+          send method, value
         else
           value
         end
-      end
-
-      #------------------
-
-      def typecast(value, klass)
-        NPR::CASTERS[klass].call(value)
-      end      
+      end   
     end # AttrTypecast
   end # Concern
 end # NPR
