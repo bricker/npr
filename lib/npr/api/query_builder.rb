c36077bda2f14608d02c2dbae7d2077aa5b2557e
Bryan Ricker <bricker88@gmail.com>
2012-12-15 02:02:22 -0800

    Add QueryBuilder #set, #to_a, #query + specs for QB

--- lib/npr/api/query_builder.rb
+++ lib/npr/api/query_builder.rb
@@ -22,20 +22,98 @@ module NPR
     
       #-----------------------
       # Build the params hash.
+      # This is automatically called by #to_a, so it probably
+      # doesn't need to be used manually.
       def to_params
-        if @_klass == NPR::Entity::Story
+        if @_klass == NPR::Entity::Story && @builder[:conditions]
           conditions = parse_conditions(@builder[:conditions])
         else
           conditions = @builder[:conditions]
         end
       
         params = conditions || {}
-      
-        params[:sort]       = @builder[:order]
-        params[:numResults] = @builder[:limit]
-        params[:startNum]   = @builder[:offset]
+        params[:sort]       = @builder[:order]  if @builder[:order]
+        params[:numResults] = @builder[:limit]  if @builder[:limit]
+        params[:startNum]   = @builder[:offset] if @builder[:offset]
+        params.merge!(@builder[:extra])         if @builder[:extra]
+        
+        params
       end
-    
+
+      #-----------------------
+      # Fire the query and return an Array of stories (or empty [])
+      #
+      # Named +to_a+ to keep in line with ActiveRecord::Relation's
+      # naming convention. It is also aliased as +query+, which 
+      # probably makes a little more sense.
+      #
+      # Returns an Array. If the query returned any stories, then
+      # it will be an array of those stories. If no stories were
+      # returned, then it will be an empty array.
+      #
+      # There is no way to access the List or Messages returned
+      # from the response. This method is meant to be a way to
+      # get to the Stories as quickly and easily as possible.
+      # If you want access to the full Response object, use #query.
+      #
+      # Example:
+      #
+      #   query = NPR::API::QueryBuilder.new(NPR::Story)
+      #   query.where(:id => [100, 150]).to_a
+      #   #=> [NPR::Story, NPR::Story]
+      #
+      #   # Assuming there is no story with an ID of 1
+      #   query.where(:id => 1).to_a
+      #   #=> []
+      #
+      # Note that for this method, +NPR.config.apiKey+ must be set.
+      #
+      def to_a
+        response = self.query
+        stories  = []
+      
+        if list = response.list
+          stories = Array.wrap(response.list.stories)
+        end
+      
+        stories
+      end
+      
+      #-----------------------
+      # Fire the query and return the full Response object
+      # 
+      # Example:
+      # 
+      #   query = NPR::API::QueryBuilder.new(NPR::Story)
+      #   query.where(:id => [100, 150]).query
+      #   #=> NPR::API::Response
+      #
+      # See NPR::API::Response for what methods are available
+      # to you.
+      #
+      # Note that for this method, +NPR.config.apiKey+ must be set.
+      def query
+        client.query(self.to_params)
+      end
+
+      #-----------------------
+      # Add in arbitrary parameters.
+      #
+      # These take precedence over anything set via
+      # any of the other builder methods.
+      #
+      # Accepts a hash of key/values that the API
+      # can receive and handle.
+      #
+      # Example:
+      #
+      #   query.set(apiKey: "YOUR_API_KEY")
+      #
+      def set(params)
+        @builder[:extra] = (@builder[:extra] || {}).merge(params)
+        self
+      end
+      
       #-----------------------
       # Merge in the passed-in conditions to what
       # already exists.
@@ -112,7 +190,11 @@ module NPR
       #-----------------------
     
       private
-    
+      
+      def client
+        @client ||= NPR::API::Client.new(:apiKey => NPR.config.apiKey)
+      end
+      
       #-----------------------
     
       def parse_conditions(conditions)
@@ -122,6 +204,7 @@ module NPR
       #-----------------------
     
       def parse_conditions!(conditions)
+        
         if conditions[:id].is_a? Array
           conditions[:id] = conditions[:id].join(",")
         end
