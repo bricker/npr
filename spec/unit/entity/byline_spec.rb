ea8219ee2ff0b37166a930304ad82bfbe88c0ac6
Bryan Ricker <bricker88@gmail.com>
2012-12-17 13:29:45 -0800

    Deeper byline implementation. Add CHANGELOG

--- spec/unit/entity/byline_spec.rb
+++ spec/unit/entity/byline_spec.rb
@@ -4,10 +4,21 @@ describe NPR::Entity::Byline do
   json_fixture do
     <<-JSON
       {
-        "id": "166481173",
+        "id": "166885535",
         "name": {
-          "$text": "Annalee Newitz"
-        }
+          "personId": "134991966",
+          "$text": "Julie Bierach"
+        },
+        "link": [
+          {
+            "type": "html",
+            "$text": "http://news.stlpublicradio.org/people/julie-bierach"
+          },
+          {
+            "type": "api",
+            "$text": "http://api.npr.org/query?id=134991966&apiKey=MDA1OTI3MjQ5MDEyODUwMTE2MzM1YzNmZA004"
+          }
+        ]
       }
     JSON
   end
@@ -18,6 +29,11 @@ describe NPR::Entity::Byline do
   
   it "sets up attributes" do
     @byline.id.should eq 166481173
-    @byline.name.should match /Annalee/
+  end
+  
+  it "creates relations" do
+    @byline.links.size.should eq 2
+    @byline.links.first.should be_a NPR::Entity::Link
+    @byline.name.should be_a NPR::Entity::Name
   end
 end
